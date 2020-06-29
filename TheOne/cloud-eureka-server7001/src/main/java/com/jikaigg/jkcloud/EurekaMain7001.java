d3f902ca5783197c5dbb18a4f518ceb351eeb2ec
Crayonjk <jikaigg@outlook.com>
2020-06-30 00:23:49 +0800

    study eureka day1

---
+++ TheOne/cloud-eureka-server7001/src/main/java/com/jikaigg/jkcloud/EurekaMain7001.java
@@ -0,0 +1,14 @@
+package com.jikaigg.jkcloud;
+
+import com.sun.deploy.security.ruleset.ExceptionRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
