6e062d33d67902b09b05e29925af062c21ea8cb4
jikaigggg <jikaigg@gmail.com>
2020-07-16 20:10:52 +0800

    no other

--- TheOne/cloud-test/src/main/java/com/jikaigg/jkcloud/string/Test1.java
+++ TheOne/cloud-test/src/main/java/com/jikaigg/jkcloud/string/Test1.java
@@ -3,13 +3,10 @@ package com.jikaigg.jkcloud.string;
 public class Test1 {
 
     public static void main(String[] args) {
-        String str1 = new String("yaojikai");
-        String str2 = new String("yaojikai");
-        System.out.println(str1.hashCode());
-        System.out.println(str2.hashCode());
-        new Object();
-
-
+        String i = "1";
+        int j = 6;
+        Object o = i+=j;
+        System.out.println(o);
+        System.out.println(o.getClass().toString());
     }
 }
-
